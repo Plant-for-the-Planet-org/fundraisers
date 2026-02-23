96e88be399259b615e09c856ecce5be865ab8eb9
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2026-02-23 14:47:03 +0530

    temp: redirect / to /explore route

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/explore',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
