4d7614e49a3fe90bfb0a291ce3c3d5a49ff31eb2
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-03-04 16:51:16 +0530

    feat(auth): prevent logged-in users from accessing login page using cookie check

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,9 +4,16 @@ import { NextResponse } from 'next/server';
 export function proxy(request: NextRequest) {
   const response = NextResponse.next();
   response.headers.set('x-pathname', request.nextUrl.pathname);
-  return response;
-}
 
+  const isAuthenticated =
+    request.cookies.get('is-authenticated')?.value === 'true';
+
+  // Prevents logged-in users accessing /login direct URL
+  if (isAuthenticated && request.nextUrl.pathname.startsWith('/login')) {
+    return NextResponse.redirect(new URL('/explore', request.url));
+  }
+  if (isAuthenticated) return response;
+}
 export const config = {
   // Run on all routes except Next.js internals and static assets
   matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)'],
