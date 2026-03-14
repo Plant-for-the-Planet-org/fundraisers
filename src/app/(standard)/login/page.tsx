19b3fd663cc376b3d307a52a0d1bffe96eaaa429
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-03-14 20:04:19 +0530

    fix: prevent authenticated users from accessing login page

--- src/app/(standard)/login/page.tsx
+++ src/app/(standard)/login/page.tsx
@@ -1,13 +1,29 @@
 'use client';
 
+import { Loader } from '@/components/ui/loader';
 import { SignInHeroImage } from '../../../components/auth/sign-in-hero-image';
 import { SignInFormPanel } from '@/components/auth/sign-in-form-panel';
 import { getSafeRedirectPath } from '@/lib/utils/auth';
-import { useSearchParams } from 'next/navigation';
+import { useAuthStore } from '@/stores/authStore';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useTranslations } from 'next-intl';
+import { DEFAULT_REDIRECT_PATH } from '@/lib/constants/auth';
 
 export default function LoginPage() {
+  const tAuth = useTranslations('Auth');
+  const router = useRouter();
   const searchParams = useSearchParams();
-  const safeRedirect = getSafeRedirectPath(searchParams.get('redirectTo'));
+  const safeRedirectPath = getSafeRedirectPath(searchParams.get('redirectTo'));
+  // store: state
+  const isAuthInitializing = useAuthStore(state => state.isAuthInitializing);
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+
+  if (isAuthInitializing) return <Loader text={tAuth('redirecting')} />;
+
+  if (isAuthenticated) {
+    router.replace(DEFAULT_REDIRECT_PATH);
+    return;
+  }
 
   return (
     <div className='flex flex-col lg:flex-row'>
@@ -15,7 +31,7 @@ export default function LoginPage() {
       <SignInHeroImage />
 
       {/* Right side - Login (order-1 on mobile, order-2 on desktop) */}
-      <SignInFormPanel redirectTo={safeRedirect} />
+      <SignInFormPanel redirectTo={safeRedirectPath} />
     </div>
   );
 }
