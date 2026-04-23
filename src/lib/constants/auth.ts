22cae9ff00b2b3f45b69635f1f05d499545e465b
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-04-23 09:42:56 +0530

    fix(auth): include /fundraisers/create in PROTECTED_PATH to avoid redirecting to protected route after logout

--- src/lib/constants/auth.ts
+++ src/lib/constants/auth.ts
@@ -1,3 +1,7 @@
 export const DEFAULT_REDIRECT_PATH = '/explore';
 
-export const PROTECTED_PATH = ['/dashboard'];
+export const PROTECTED_PATH = [
+  '/dashboard',
+  '/dashboard/fundraisers/edit',
+  '/fundraisers/create',
+];
