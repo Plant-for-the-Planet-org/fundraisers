b15f3a48e39f2038142e11fa199680ea44d3ce40
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-06-30 20:19:05 +0530

    fix: update support project name

--- src/lib/constants/project-selection.ts
+++ src/lib/constants/project-selection.ts
@@ -12,7 +12,7 @@ export const DEFAULT_NON_EARMARKED_CAUSE_BY_COUNTRY: Readonly<DefaultCauseIdByCo
   };
 
 export const DEFAULT_NON_EARMARKED_CAUSE_FALLBACK = {
-  name: 'Empower Young People and Bring Back Forests',
+  name: 'Education for Climate Justice',
   description:
     "Support Plant-for-the-Planet's mission to empower young people worldwide to take climate action. We restore forests, educate communities, and advocate for climate justice to create a sustainable future for all.",
   image: '6852bd2495cde981886580.png',
